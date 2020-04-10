1300599346f4ed407f43e9a88be2bad6671b8ffa
jeongwoo <chlwjddn9181@naver.com>
2020-04-10 13:17:49 +0900

    complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ package com.company;
 import java.util.*;
 
 /*
-*
+*g
 * */
 public class Main {
 
@@ -12,44 +12,38 @@ public class Main {
         // write your code here
 
         //입력
+        Scanner sc = new Scanner(System.in);
+
+            for(int i =1; i<=10;i++){
+                String input = sc.nextLine();
+                int dumpNum = Integer.parseInt(input);
+
+                int [] boxes = new int[100];
+                String [] dummy = sc.nextLine().split(" ");
 
 
+                for(int j =0; j<100; j++){
+                    boxes[j] = Integer.parseInt(dummy[j]);
+                }
+                Arrays.sort(boxes);
 
-        for (int i = 1; i <= 10; i++) {
-            Scanner sc = new Scanner(System.in);
+                while(dumpNum>0){
+                    //제일 쉽게 짤 수 있는 g방법 --> 제일 위쪽에서 뺴서 제일 앞쪽에 더하는것
+                    boxes[0]++;
+                    boxes[99]--;
+                    dumpNum--;
+                    Arrays.sort(boxes);
 
-            int num  = sc.nextInt();
-
-            int[] buildings = new int[num];
-            int answer = 0;
-
-            for (int j = 0; j < num; j++) {
-                int height = sc.nextInt();
-                buildings[j] = height;
-
-            }
-
-                for (int k = 0; k < num; k++) {
-                    //k가 array의 인덱스를 초과하면 안되기 때문에 continue;
-                    if (k - 2 < 0 || k + 2 >= num) {
-                        continue;
-                    } else {
-                        //왼쪽, 오른쪽을 비교, 비교 방법은 왼쪽과 오른쪽 2개의 높이중 가장 큰 높이를 선택 후 선택한 높이에서 뺸다.
-                        int currentHeight = buildings[k];
-                        int left = Math.max(buildings[k - 1], buildings[k - 2]);
-                        int right = Math.max(buildings[k + 1], buildings[k + 2]);
-                        int bestHeightBuilding = Math.max(left, right);
-
-                        if (currentHeight - bestHeightBuilding > 0) {
-                            answer += currentHeight - bestHeightBuilding;
-
-                        }
-
+                    if(boxes[0]==boxes[99]){
+                        break;
                     }
 
+                    if(boxes[0]+1==boxes[99]){
+                        break;
+                    }
                 }
-
-            System.out.println("# "+i+" "+answer);
+                int answer = boxes[99]-boxes[0];
+                System.out.println("#"+i+" "+ answer);
             }
 
         }
