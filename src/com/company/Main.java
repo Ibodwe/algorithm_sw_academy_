076b1a8921735a9be5dd0884d427a4b489ec06ef
jeongwoo <chlwjddn9181@naver.com>
2020-04-13 15:36:35 +0900

    BaekJoon 13458_Samsung 기출

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,51 +1,53 @@
 package com.company;
 
 
+import javax.print.attribute.HashAttributeSet;
 import java.util.*;
 
-/*
-*g
-* */
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
-        //입력
         Scanner sc = new Scanner(System.in);
 
-            for(int i =1; i<=10;i++){
-                String input = sc.nextLine();
-                int dumpNum = Integer.parseInt(input);
+        int n = sc.nextInt();
+        int [] people = new int[n];
 
-                int [] boxes = new int[100];
-                String [] dummy = sc.nextLine().split(" ");
+        for(int i =0; i<n; i++){
+            people[i] = sc.nextInt();
+        }
 
+        int B = sc.nextInt();
+        int C = sc.nextInt();
 
-                for(int j =0; j<100; j++){
-                    boxes[j] = Integer.parseInt(dummy[j]);
+        long answer = 0L;
+
+        for(int i =0; i<n ; i++){
+
+            int currentPeople = people[i];
+
+            if(B >= currentPeople){
+                answer+=1;
+            }else{
+                answer++;
+                currentPeople -= B;
+
+                long number = currentPeople / C;
+                long dive = currentPeople % C;
+
+                if(dive>0){
+                    answer+=1;
                 }
-                Arrays.sort(boxes);
+                answer = (long)answer + number;
 
-                while(dumpNum>0){
-                    //제일 쉽게 짤 수 있는 g방법 --> 제일 위쪽에서 뺴서 제일 앞쪽에 더하는것
-                    boxes[0]++;
-                    boxes[99]--;
-                    dumpNum--;
-                    Arrays.sort(boxes);
-
-                    if(boxes[0]==boxes[99]){
-                        break;
-                    }
-
-                    if(boxes[0]+1==boxes[99]){
-                        break;
-                    }
-                }
-                int answer = boxes[99]-boxes[0];
-                System.out.println("#"+i+" "+ answer);
             }
 
         }
+
+        System.out.println((long)answer);
+
     }
 
+
+}
