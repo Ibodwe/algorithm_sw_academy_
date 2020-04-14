bbfdd5e49f0e018af8ecdcf48a9e71b821ba1dd3
jeongwoo <chlwjddn9181@naver.com>
2020-04-14 19:54:20 +0900

    BaekJoon 15686_Samsung 기출

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,122 +8,13 @@ import java.util.*;
 public class Main {
 
 
-    static int n =0;
-    static int m = 0;
-    static int answer= 0;
-    static int [][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
-    static int [][] temp;
-    static Stack<Integer> xSt = new Stack<Integer>();
-    static Stack<Integer> ySt = new Stack<Integer>();
-
-    static public void solution(int count, int [][]map){
-
-        if(count==3){
-            //체크하는 코드
-            for(int i =0; i<n; i++){
-                for(int j =0; j<m; j++){
-                    temp[i][j] = map[i][j];
-                }
-            }
-
-            checkMap(temp);
-
-            return;
-        }
-
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                if(map[i][j]==0){
-                    map[i][j] = 1;
-                    solution(count+1,map);
-                    map[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    static public void checkMap(int [][]valueMap){
-        boolean change = true;
-
-        while(change){
-         change=false;
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                if(valueMap[i][j]==2){
-                    for(int k=0; k<4; k++){
-
-                        int x = direction[k][0] + i;
-                        int y = direction[k][1] + j;
-
-                        if(checkValue(x,y)){
-                            if(valueMap[x][y]==0){
-                            change = true;
-                            xSt.add(x);
-                            ySt.add(y);
-                            }
-                        }
-                    }
-
-                }
-            }
-        }
-
-        while(!ySt.isEmpty()){
-            int a = xSt.peek();
-            int b = ySt.peek();
-            valueMap[a][b] = 2;
-            xSt.pop();
-            ySt.pop();
-        }
-
-        if(!change){
-            int count = 0;
-            for(int i =0; i<n; i++){
-                for(int j =0; j<m; j++){
-                    if(valueMap[i][j]==0){
-                        count++;
-                    }
-                }
-            }
-            answer = Math.max(answer,count);
-        }
-
-        }
-
-    }
-
-    static public boolean checkValue(int a, int b){
-
-        if(a < 0 || a >= n || b < 0 || b >= m){
-            return false;
-        }
-
-        return true;
-    }
-
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        //현재방향
+        //후진
 
-         n = sc.nextInt();
-         m = sc.nextInt();
-
-        int [][] maps = new int [n][m];
-            temp = new int[n][m];
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                maps[i][j] = sc.nextInt();
-            }
-        }
-        solution(0 , maps);
-
-
-
-        System.out.println(answer);
-        // 0에 최대한 i를 배치 하고
-        // 바이러스가 전부 퍼질때 까지 배치
-        //
 
     }
 
---
+++ src/com/company/SamSung_15686_BaekJoon.java
@@ -0,0 +1,97 @@
+package com.company;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.*;
+
+public class SamSung_15686_BaekJoon {
+
+   static ArrayList<xandy> chicken = new ArrayList<xandy>();
+
+   static ArrayList<xandy> house = new ArrayList<xandy>();
+   static ArrayList<xandy> sample = new ArrayList<xandy>();
+
+    static int chickenDistance = 999999;
+
+  static public class xandy{
+        int x;
+        int y;
+        public xandy(int a, int b){
+            x =a ;
+            y = b;
+        }
+    }
+
+    static public void selectChicken(int m, int count, int index ){
+        int value =0;
+      if(count==m){
+          //선택 완료
+
+          for(int j=0; j< sample.size(); j++){
+              System.out.print(sample.get(j).x +" " + sample.get(j).y);
+              System.out.println();
+          }
+          System.out.println();
+
+
+
+
+          for(int i=0; i<house.size(); i++){
+              int temp = 999999;
+              for(int j=0; j<sample.size(); j++){
+                  int x = calDistance(house.get(i).x, house.get(i).y, sample.get(j).x, sample.get(j).y);
+                  temp = Math.min(x,temp);
+              }
+              value += temp;
+          }
+
+          chickenDistance = Math.min(chickenDistance,value);
+
+          return;
+      }
+
+      for(int i =index; i< chicken.size();i++ ){
+            sample.add(chicken.get(i));
+            selectChicken(m,count+1,i+1);
+            sample.remove(chicken.get(i));
+      }
+
+    }
+
+    static public int calDistance(int x1, int y1, int x2, int y2){
+        int a = Math.abs(x1-x2);
+        int b = Math.abs(y1-y2);
+      return a+b;
+    }
+
+
+
+    public static void main(String args[] ){
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+
+
+
+        int[][] map = new int [n][n];
+
+        for(int i =0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int num = sc.nextInt();
+
+                if(num==1){
+                    house.add(new xandy(i,j));
+                }
+                if(num==2){
+                    chicken.add(new xandy(i,j));
+                }
+            }
+        }
+
+        selectChicken(m,0,0);
+        System.out.println(chickenDistance);
+    }
+}
